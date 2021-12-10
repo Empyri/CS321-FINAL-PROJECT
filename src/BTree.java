95c87962739efa71184599293bb29fea9ef68b5c
Empyri <Liquidleneyes@gmail.com>
2021-12-09 17:49:34 -0700

    Added TreeObject.java and BTreeNode.java. Created class for Btree. No work in it yet.

---
+++ src/BTree.java
@@ -0,0 +1,9 @@
+public class BTree {
+    private int M;
+    private BTreeNode root;
+
+    // min children - internal nodes = M/2
+    //root has min children of 0
+    //min keys - root node - 1
+    //min keys - all other nodes - (M/2)-1
+}
